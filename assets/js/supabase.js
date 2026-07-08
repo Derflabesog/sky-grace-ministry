2f9d0868c412a062bdd520748217fc2f9dd99e4a
Derflabesog <wildernesafrica@gmail.com>
2026-07-08 09:30:21 +0000

    Expose Supabase client globally

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -2,7 +2,7 @@ const SUPABASE_URL = "https://mkdngtbrwuwleacezude.supabase.co";
 
 const SUPABASE_ANON_KEY = "sb_publishable_gNDowcDvO1Y4KUS4iaw4eA_VLES7dEC";
 
-const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
+window.supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
 );
