be5f134ae36ed3c8d3ed78473e0bcb659b6b30e8
Derflabesog <wildernesafrica@gmail.com>
2026-07-08 09:49:55 +0000

    Fix global Supabase client name

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -2,7 +2,9 @@ const SUPABASE_URL = "https://mkdngtbrwuwleacezude.supabase.co";
 
 const SUPABASE_ANON_KEY = "sb_publishable_gNDowcDvO1Y4KUS4iaw4eA_VLES7dEC";
 
-window.supabaseClient = supabase.createClient(
+const supabaseClient = supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
+
+window.supabaseClient = supabaseClient;
